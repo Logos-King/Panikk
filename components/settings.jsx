03b4b4bd94a752ff6db438f9c713b3ea9498553a
LandenStephenss <turtlederp806@icloud.com>
2021-05-22 16:12:15 -0500

    hopefully this doesn't break a ton of user's discord

--- components/settings.jsx
+++ components/settings.jsx
@@ -1,26 +1,42 @@
 const { React } = require("powercord/webpack");
 const { FormTitle } = require("powercord/components");
 const { SwitchItem, RadioGroup } = require("powercord/components/settings");
-
+const KeybindRecorder = require("./KeybindRecorder");
 module.exports = class Panikk extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      onLive: this.props.getSetting("panikkOnLive", true)
+      onLive: this.props.getSetting("panikkOnLive", true),
     };
   }
 
   render() {
+    const { updateSetting, getSetting } = this.props;
     return (
       <>
+        <KeybindRecorder
+          value={getSetting("keybind", "F5")}
+          onChange={(e) => {
+            this.setState({ value: e });
+            updateSetting("keybind", e);
+          }}
+          onReset={() => {
+            this.setState({ value: "F5" });
+            updateSetting("keybind", "F5");
+          }}
+        >
+          Toggle Keybind
+        </KeybindRecorder>
+
         <SwitchItem
-          note='Panikk on live'
+          note="Panikk on live"
           value={this.state.onLive}
           onChange={() => {
             this.setState({ onLive: !this.state.onLive });
             this.props.toggleSetting("panikkOnLive");
-          }}>
+          }}
+        >
           Enable
         </SwitchItem>
       </>
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 /* eslint-disable quotes */
 const { Plugin } = require("powercord/entities");
 const webpack = require("powercord/webpack");
+const { inject, uninject } = require("powercord/injector");
 let suppress = false;
 const Settings = require("./components/settings");
+
 module.exports = class Panikk extends Plugin {
   async startPlugin() {
     const dispatcher = webpack.getModule(["dispatch"], false);
     powercord.api.settings.registerSettings(this.entityID, {
       category: this.entityID,
       label: "Panikk",
-      render: (props) =>
-        webpack.React.createElement(Settings, {
-          main: this,
-          ...props
-        })
+      render: Settings,
     });
     dispatcher.subscribe("STREAM_START", async () => {
       if (!this.settings.get("panikkOnLive", false)) {
@@ -32,17 +30,19 @@ module.exports = class Panikk extends Plugin {
       suppress = false;
     });
     document.body.removeEventListener("keyup", this.keyup);
-    document.body.addEventListener("keyup", this.keyup);
+    document.body.addEventListener("keyup", (e) => this.keyup(e, this));
   }
 
   pluginWillUnload() {
     if (!suppress) {
       document.body.removeEventListener("keyup", this.keyup);
     }
+    powercord.api.settings.unregisterSettings(this.entityID);
   }
 
-  async keyup(event) {
-    if (event.key === "F5") {
+  async keyup(event, _this) {
+    var key = _this.settings.get("keybind", "F5");
+    if (event.key === key) {
       if (powercord?.api?.settings?.ready === true) {
         suppress = true;
         await powercord.shutdown();
